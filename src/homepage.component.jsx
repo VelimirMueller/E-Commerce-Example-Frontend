3995752b0ec43b87d9c001a8bb2ecb6069481f6a
unknown <velimir.mueller@googlemail.com>
2020-12-21 23:41:24 +0100

    Hello World

---
+++ src/homepage.component.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+
+      <div className='menu-item'>
+        <div classNAme='content'>
+          <h1 className='title'>First Item</h1>
+          <span className='subtitle'>Shop Now!</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div classNAme='content'>
+          <h1 className='title'>Second Item</h1>
+          <span className='subtitle'>Shop Now!</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div classNAme='content'>
+          <h1 className='title'>Third Item</h1>
+          <span className='subtitle'>Shop Now!</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div classNAme='content'>
+          <h1 className='title'>Fourth Item</h1>
+          <span className='subtitle'>Shop Now!</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div classNAme='content'>
+          <h1 className='title'>Fifth Item</h1>
+          <span className='subtitle'>Shop Now!</span>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+);
+
+export default HomePage;
