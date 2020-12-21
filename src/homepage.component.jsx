c39cd9250c3495bcd7e9f6c797fdec2f3953ef90
unknown <velimir.mueller@googlemail.com>
2020-12-22 00:06:42 +0100

    create react app project and first homepage layout

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -4,40 +4,40 @@ import './homepage.styles.scss'
 
 const HomePage = () => (
   <div className='homepage'>
-    <div className='directory-menu'>
+    <div className='main-menu'>
 
-      <div className='menu-item'>
-        <div classNAme='content'>
-          <h1 className='title'>First Item</h1>
-          <span className='subtitle'>Shop Now!</span>
+      <div className='menu-i'>
+        <div className='cont'>
+          <h2 className='title'>First Item</h2>
+          <span className='subt'>Shop Now!</span>
         </div>
       </div>
 
-      <div className='menu-item'>
-        <div classNAme='content'>
-          <h1 className='title'>Second Item</h1>
-          <span className='subtitle'>Shop Now!</span>
+      <div className='menu-i'>
+        <div className='cont'>
+          <h2 className='title'>Second Item</h2>
+          <span className='subt'>Shop Now!</span>
         </div>
       </div>
 
-      <div className='menu-item'>
-        <div classNAme='content'>
-          <h1 className='title'>Third Item</h1>
-          <span className='subtitle'>Shop Now!</span>
+      <div className='menu-i'>
+        <div className='cont'>
+          <h2 className='title'>Third Item</h2>
+          <span className='subt'>Shop Now!</span>
         </div>
       </div>
 
-      <div className='menu-item'>
-        <div classNAme='content'>
-          <h1 className='title'>Fourth Item</h1>
-          <span className='subtitle'>Shop Now!</span>
+      <div className='menu-i'>
+        <div className='cont'>
+          <h2 className='title'>Fourth Item</h2>
+          <span className='subt'>Shop Now!</span>
         </div>
       </div>
 
-      <div className='menu-item'>
-        <div classNAme='content'>
-          <h1 className='title'>Fifth Item</h1>
-          <span className='subtitle'>Shop Now!</span>
+      <div className='menu-i'>
+        <div className='cont'>
+          <h2 className='title'>Fifth Item</h2>
+          <span className='subt'>Shop Now!</span>
         </div>
       </div>
       
